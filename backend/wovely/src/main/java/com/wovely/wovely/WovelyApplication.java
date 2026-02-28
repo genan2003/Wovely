d3a06518b10f88c443097531c17da2097cd79a35
Genan Omer <genanomer99@gmail.com>
2026-03-01 01:13:47 +0200

    add initial setup

---
+++ backend/wovely/src/main/java/com/wovely/wovely/WovelyApplication.java
@@ -0,0 +1,13 @@
+package com.wovely.wovely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WovelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WovelyApplication.class, args);
+	}
+
+}
---
+++ backend/wovely/src/test/java/com/wovely/wovely/WovelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wovely.wovely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WovelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
